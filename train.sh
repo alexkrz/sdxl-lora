ce94496f53b50f68ae77c2c6a9add23f4244efa5
Alexander Kurz <alex.kurz95@gmail.com>
2026-05-11 11:07:47 +0200

    Try different training hyperparameters

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Parameters from notebook
-output_dir="training_logs/3d-icon-sdxl-lora-v2"  # Need to verify that slashes work correctly in output_dir
+output_dir="training_logs/3d-icon-sdxl-lora-v5"  # Need to verify that slashes work correctly in output_dir
 instance_prompt="3d icon in the style of TOK"
 validation_prompt="a TOK icon of an astronaut riding a horse, in the style of TOK"
 rank=8
@@ -12,6 +12,7 @@ accelerate launch train_dreambooth_lora_sdxl_advanced.py \
   --dataset_name="./data/3d_icon" \
   --instance_prompt="$instance_prompt" \
   --validation_prompt="$validation_prompt" \
+  --validation_epochs=50 \
   --output_dir="$output_dir" \
   --caption_column="prompt" \
   --mixed_precision="bf16" \
@@ -21,17 +22,19 @@ accelerate launch train_dreambooth_lora_sdxl_advanced.py \
   --report_to="wandb"\
   --gradient_accumulation_steps=1 \
   --gradient_checkpointing \
+  --optimizer="prodigy"\
   --learning_rate=1.0 \
   --text_encoder_lr=1.0 \
+  --adam_beta1=0.9 \
   --adam_beta2=0.99 \
-  --optimizer="prodigy"\
+  --adam_weight_decay=0.01 \
   --train_text_encoder_ti\
   --train_text_encoder_ti_frac=0.5\
   --snr_gamma=5.0 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --rank="$rank" \
-  --max_train_steps=1500 \
-  --checkpointing_steps=2000 \
-  --seed="0" \
+  --max_train_steps=2000 \
+  --checkpointing_steps=500 \
+  --seed=42 \
   # --push_to_hub
